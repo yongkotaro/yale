a80026b733cd32293ae74cfe6301895ca99cfad6
yongkotaro <yongkotaro@gmail.com>
2024-11-12 20:09:31 +0800

    Add admissions section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from './navbar/navbar.component'; 
 import { ProgramComponent } from './program/program.component'; 
+import { AdmissionComponent } from './admission/admission.component';
+import { EventComponent } from './event/event.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [NavbarComponent, ProgramComponent], 
+  imports: [NavbarComponent, ProgramComponent, AdmissionComponent, EventComponent], 
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'] 
 })
