07956fe850667b4c6166591f6388cbb649dba661
yongkotaro <yongkotaro@gmail.com>
2024-11-12 18:55:53 +0800

    Program section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from './navbar/navbar.component'; 
+import { ProgramComponent } from './program/program.component'; 
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [NavbarComponent], 
+  imports: [NavbarComponent, ProgramComponent], 
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'] 
 })
